58b74e01b8c2c8ff61f35a1e1ca79ce53e38d470
code-driving <tittan7@gmail.com>
2021-03-11 20:40:19 +0000

    initial code unchanged

---
+++ myBreedFetcherOriginal.js
@@ -0,0 +1,39 @@
+const request = require("request");
+const userInput = process.argv.slice(2)[0];
+// console.log(userInput)
+
+//create a function to fetch a breed
+//in there use a request to make a request to an api
+//check if the status code is successful
+//handle errors
+//parse the data from the JSON
+const fetchCatsBreed = (breed, callback) => {
+  //making the breed search dynamic
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
+
+  request(url, (error, response, body) => {
+    //if system error occurs
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    if (response.statusCode !== 200) {
+      callback("Bad request", null);
+      return;
+    }
+    const data = JSON.parse(body);
+    if (!data.length) {
+      callback("Breed not found", null);
+      return;
+    }
+    callback(null, data[0].description);
+  });
+};
+
+fetchCatsBreed(userInput, (error, data) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Success");
+  }
+});
