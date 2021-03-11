ae04fc68183a7efd1fbbaf03edea1fff6ec95181
code-driving <tittan7@gmail.com>
2021-03-11 20:42:15 +0000

    creating index.js as per instructions

---
+++ index.js
@@ -0,0 +1,10 @@
+const { fetchCatsBreed } = require("./breedFetcherModified");
+const userInput = process.argv.slice(2)[0];
+
+fetchCatsBreed(userInput, (error, desc) => {
+  if (error) {
+    console.log('Error fetch details:', error);
+  } else {
+    console.log(desc);
+  }
+});
